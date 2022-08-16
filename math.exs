6f30501c68a52fb97468953a776ab63556e695ed
Tyler Lego <tylerlego15@gmail.com>
2022-08-15 21:32:26 -0400

    intro to processes

--- math.exs
+++ math.exs
@@ -3,8 +3,24 @@ defmodule Math do
     a + b
   end
 
+  def sum_list([head | tail], accumulator) do
+    sum_list(tail, accumulator + head)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
+
   def multiply(x, n) do
     x * n
+  end
+
+  def double_each([head | tail], accumulator) do
+    [head * 2 | double_each(tail)]
+  end
+
+  def double_each([], accumulator) do
+    []
   end
 
   def zero?(0) do
@@ -15,3 +31,9 @@ defmodule Math do
     false
   end
 end
+
+# Math.sum_list([1,2,3], 0)
+# Enum.reduce ([1,2,3], 0, fn(x, acc) -> x + acc end)
+
+# Math.double_each([1,2,3], 0)
+# Enum.map ([1,2,3], fn(x) -> x * 2 end)
